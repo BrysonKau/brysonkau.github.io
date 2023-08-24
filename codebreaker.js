4ad9c1eb4952541035623cb6ee4b0eaf0415d27b
BrysonKau <132340343+BrysonKau@users.noreply.github.com>
2023-08-23 21:05:35 -1000

    Update codebreaker.js

--- codebreaker.js
+++ codebreaker.js
@@ -21,3 +21,34 @@ function numberStringToLetters(numberString) {
   
     return result;
   }
+
+  function generateCode(){
+    const inputString = document.getElementById('inputString').value;
+    const crypticMessage = convertStringToNumbers(inputString);
+    document.getElementById('outputMessage').textContent = crypticMessage;
+  }
+  
+  function convertStringToNumbers(inputString) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const lowercaseInput = inputString.toLowerCase();
+    let result = '';
+  
+    for (let i = 0; i < lowercaseInput.length; i++) {
+      const char = lowercaseInput[i];
+      const index = alphabet.indexOf(char);
+      
+      if (index !== -1) {
+        result += (index + 1).toString();
+      } else {
+        result += char;
+      }
+  
+      if (i !== lowercaseInput.length - 1) {
+        result += '-';
+      }
+    }
+  
+    return result;
+  }
+
+  
